69b36297c08278ddaaf398dfbf6c4127310708f4
ahm3tcelik <ahm3tcelik72@gmail.com>
2021-04-22 21:12:20 +0300

    Add localization manager

--- furniture_mobile_app/lib/core/init/lang/language_manager.dart
+++ furniture_mobile_app/lib/core/init/lang/language_manager.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+// pub run easy_localization:generate -S "assets/lang" -f keys -O "lib/core/init/lang" -o locale_keys.g.dart
+
 class LanguageManager {
   static LanguageManager? _instance;
   static LanguageManager get instance {
---
+++ furniture_mobile_app/lib/core/init/lang/locale_keys.g.dart
@@ -0,0 +1,9 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const welcome = 'welcome';
+  static const widgets_product_card_item_buy = 'widgets.product_card_item.buy';
+  static const widgets_product_card_item = 'widgets.product_card_item';
+  static const widgets = 'widgets';
+
+}
