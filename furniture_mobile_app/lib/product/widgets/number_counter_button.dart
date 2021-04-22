cda4e8605a85a82ff4fb0fcd620d28fd48f52f28
ahm3tcelik <ahm3tcelik72@gmail.com>
2021-04-23 01:48:31 +0300

    Set number counter widget to statefull

--- furniture_mobile_app/lib/product/widgets/number_counter_button.dart
+++ furniture_mobile_app/lib/product/widgets/number_counter_button.dart
@@ -2,72 +2,92 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kartal/kartal.dart';
 
-class NumberCounterButton extends StatelessWidget {
-
-  final int value;
-  final VoidCallback? onIncrease;
-  final VoidCallback? onDecrease;
+class NumberCounterButton extends StatefulWidget {
+  final Function(int)? onChanged;
   final double? width;
   final double? height;
+  final int minValue;
 
   const NumberCounterButton({
     Key? key,
-    required this.value,
-    this.onIncrease,
-    this.onDecrease,
+    this.onChanged,
     this.width,
     this.height,
+    this.minValue = 1
   }) : super(key: key);
+
+  @override
+  _NumberCounterButtonState createState() => _NumberCounterButtonState();
+}
+
+class _NumberCounterButtonState extends State<NumberCounterButton> {
+
+  int value = 1;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: height ?? 30,
-      width: width ?? 100,
+      height: widget.height ?? 30,
+      width: widget.width ?? 100,
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Expanded(
-            flex: 1,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: context.lowRadius,
-                  bottomLeft: context.lowRadius,
-                ),
-                color: Colors.grey
-              ),
-              child: InkWell(
-                onTap: onIncrease,
-                child: Icon(Icons.remove, color: context.colorScheme.onPrimary),
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              alignment: Alignment.center,
-              color: context.appTheme.scaffoldBackgroundColor,
-              child: Text('$value', style: context.textTheme.headline6,),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topRight: context.lowRadius,
-                    bottomRight: context.lowRadius,
-                  ),
-                  color: context.appTheme.primaryColor
-              ),
-              child: InkWell(
-                onTap: onDecrease,
-                child: Icon(Icons.add, color: context.colorScheme.onPrimary),
-              ),
-            ),
-          ),
+          Expanded(flex: 1, child: _buildDecrease(context)),
+          Expanded(flex: 2, child: _buildShowValue(context)),
+          Expanded(flex: 1, child: _buildIncrease(context)),
         ],
+      ),
+    );
+  }
+
+  Widget _buildDecrease(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: context.lowRadius,
+            bottomLeft: context.lowRadius,
+          ),
+          color: Colors.grey),
+      child: InkWell(
+        onTap: () {
+          if (value > widget.minValue) {
+            setState(() {
+              value--;
+              widget.onChanged?.call(value);
+            });
+          }
+        },
+        child: Icon(Icons.remove, color: context.colorScheme.onPrimary),
+      ),
+    );
+  }
+
+  Widget _buildIncrease(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topRight: context.lowRadius,
+            bottomRight: context.lowRadius,
+          ),
+          color: context.appTheme.primaryColor),
+      child: InkWell(
+        onTap: () {
+          setState(() {
+            value += 1;
+            widget.onChanged?.call(value);
+          });
+        },
+        child: Icon(Icons.add, color: context.colorScheme.onPrimary),
+      ),
+    );
+  }
+
+  Widget _buildShowValue(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      color: context.appTheme.scaffoldBackgroundColor,
+      child: Text('$value',
+        style: context.textTheme.headline6,
       ),
     );
   }
