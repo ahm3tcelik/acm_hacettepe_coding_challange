a6972a08e5c733d1b60623b5ecfa3ba43a7957fb
ahm3tcelik <ahm3tcelik72@gmail.com>
2021-04-23 23:27:31 +0300

    Uodate translate

--- message_app/lib/core/init/lang/locale_keys.g.dart
+++ message_app/lib/core/init/lang/locale_keys.g.dart
@@ -2,5 +2,18 @@
 
 abstract class  LocaleKeys {
   static const app_title = 'app_title';
+  static const app_name = 'app_name';
+  static const activity = 'activity';
+  static const messages = 'messages';
+  static const call = 'call';
+  static const camera = 'camera';
+  static const settings = 'settings';
+  static const online = 'online';
+  static const offline = 'offline';
+  static const now = 'now';
+  static const min = 'min';
+  static const day = 'day';
+  static const hour = 'hour';
+  static const type_message = 'type_message';
 
 }
